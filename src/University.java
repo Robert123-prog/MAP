3afeb55e51daa5d1ef23101ff29cdb3d4e62b6fb
OOP <roeminovici@gmail.com>
2024-10-07 11:09:46 +0300

    average method commit

--- src/University.java
+++ src/University.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 
+//AUFGABE 1
 public class University {
     int treshold = 40;
+
 
     public ArrayList<Integer> insufficient(ArrayList<Integer> grades){
         ArrayList<Integer> insufficientGrades = new ArrayList<>();
@@ -14,6 +16,18 @@ public class University {
 
         return insufficientGrades;
     }
+
+    public double average(ArrayList<Integer> grades){
+        int sum = 0;
+
+        for (int i = 0; i < grades.size(); i++){
+            sum += grades.get(i);
+        }
+
+        double averageGrade = sum / grades.size();
+        return averageGrade;
+    }
+
 
 
 }
