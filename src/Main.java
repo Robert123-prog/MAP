b07d419eb9a8d686a66137948af991545762dca4
OOP <roeminovici@gmail.com>
2024-10-08 23:17:51 +0300

    final commit;added best combination method and helper methods

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,30 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-//        University university = new University();
-//        int[] grades = {13, 46, 43, 99, 89, 73, 87};
-//
-//        System.out.println(Arrays.toString(university.insufficient(grades)));
-//        System.out.println(university.average(grades));
-//        System.out.println(Arrays.toString(university.roundedUp(grades)));
-//        System.out.println(university.maximumRoundedUp(grades));
-//
-//        ArrayOperations ops = new ArrayOperations(5, new int[]{4, 8, 3, 10, 17});
-//
-//        System.out.println(ops.minimum());
-//        System.out.println(ops.maximum());
-//        System.out.println(ops.maxSum());
-//        System.out.println(ops.minSum());
-//
-//        BigNumbers bn = new BigNumbers(new int[] {8, 7, 6, 4, 0, 0, 0}, new int[] {8, 7, 6, 2, 0, 0, 0});
-//        System.out.println(Arrays.toString(bn.sum()));
-//        System.out.println(Arrays.toString(bn.difference()));
-//
-//        BigNumbers bn2 = new BigNumbers(new int[]{2, 3, 6, 0, 0, 0, 0, 0, 0}, new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1});
-//        System.out.println(Arrays.toString(bn2.multiplication(2)));
-//
-//        BigNumbers bn3 = new BigNumbers(new int[]{2, 3, 6, 0, 0, 0, 0, 0, 0}, new int[]{3, 3, 3, 3, 3, 3, 3, 3, 3});
-//        System.out.println(Arrays.toString(bn3.division(2)));
+        University university = new University();
+        int[] grades = {13, 46, 43, 99, 89, 73, 87};
+
+        System.out.println(Arrays.toString(university.insufficient(grades)));
+        System.out.println(university.average(grades));
+        System.out.println(Arrays.toString(university.roundedUp(grades)));
+        System.out.println(university.maximumRoundedUp(grades));
+
+        ArrayOperations ops = new ArrayOperations(5, new int[]{4, 8, 3, 10, 17});
+
+        System.out.println(ops.minimum());
+        System.out.println(ops.maximum());
+        System.out.println(ops.maxSum());
+        System.out.println(ops.minSum());
+
+        BigNumbers bn = new BigNumbers(new int[] {8, 7, 6, 4, 0, 0, 0}, new int[] {8, 7, 6, 2, 0, 0, 0});
+        System.out.println(Arrays.toString(bn.sum()));
+        System.out.println(Arrays.toString(bn.difference()));
+
+        BigNumbers bn2 = new BigNumbers(new int[]{2, 3, 6, 0, 0, 0, 0, 0, 0}, new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1});
+        System.out.println(Arrays.toString(bn2.multiplication(2)));
+
+        BigNumbers bn3 = new BigNumbers(new int[]{2, 3, 6, 0, 0, 0, 0, 0, 0}, new int[]{3, 3, 3, 3, 3, 3, 3, 3, 3});
+        System.out.println(Arrays.toString(bn3.division(2)));
 
         Markus m = new Markus();
         int[] keyboards = new int[]{40, 35, 70, 15, 45};
@@ -36,7 +36,11 @@ public class Main {
         System.out.println(m.cheapestKeyboard(keyboards));
         System.out.println(m.mostExpensiveItems(keyboards2, usbs));
         System.out.println(m.mostExpensiveUsbInBudget(30, usbs2));
+        System.out.println(m.mostExpensiveKeyboardInBudget(5, keyboards));
 
+        System.out.println(m.bestCombinationInBudget(60, new int[]{40, 50, 60}, new int[]{8, 12}));
+        System.out.println(m.bestCombinationInBudget(60, new int[]{60}, new int[]{8, 12}));
+        System.out.println(m.bestCombinationInBudget(60, new int[]{40, 60}, new int[]{8, 12}));
 
     }
 }
--- src/Markus.java
+++ src/Markus.java
@@ -1,27 +1,27 @@
 public class Markus {
 
-    public int cheapestKeyboard(int[] keyboardPrices){
+    public int cheapestKeyboard(int[] keyboardPrices) {
         int minPrice = Integer.MAX_VALUE;
 
-        for (int keyboardPrice: keyboardPrices){
-            if (keyboardPrice < minPrice){
+        for (int keyboardPrice : keyboardPrices) {
+            if (keyboardPrice < minPrice) {
                 minPrice = keyboardPrice;
             }
         }
         return minPrice;
     }
 
-    public int mostExpensiveItems(int[] keyboardPrices, int[] usbPrices){
+    public int mostExpensiveItems(int[] keyboardPrices, int[] usbPrices) {
         int maxPrice = Integer.MIN_VALUE;
 
-        for (int keyboardPrice: keyboardPrices){
-            if (keyboardPrice > maxPrice){
+        for (int keyboardPrice : keyboardPrices) {
+            if (keyboardPrice > maxPrice) {
                 maxPrice = keyboardPrice;
             }
         }
 
-        for (int usbPrice: usbPrices){
-            if (usbPrice > maxPrice){
+        for (int usbPrice : usbPrices) {
+            if (usbPrice > maxPrice) {
                 maxPrice = usbPrice;
             }
         }
@@ -29,12 +29,12 @@ public class Markus {
         return maxPrice;
     }
 
-    public int mostExpensiveUsbInBudget(int budget, int[] usbPrices){
+    public int mostExpensiveUsbInBudget(int budget, int[] usbPrices) {
         int minDifference = Integer.MAX_VALUE;
         int usbInBudget = 0;
 
-        for (int usbPrice: usbPrices){
-            if (Math.abs(budget - usbPrice) < minDifference){
+        for (int usbPrice : usbPrices) {
+            if (Math.abs(budget - usbPrice) < minDifference) {
                 minDifference = budget - usbPrice;
                 usbInBudget = usbPrice;
             }
@@ -42,4 +42,55 @@ public class Markus {
         return usbInBudget;
     }
 
+    //helper method
+    public int mostExpensiveKeyboardInBudget(int budget, int[] keyboardPrices) {
+        if (budget < this.cheapestKeyboard(keyboardPrices)) {
+            return -1;
+        }
+
+        int minDifference = Integer.MAX_VALUE;
+        int keyboardInBudget = 0;
+
+        for (int keyboardPrice : keyboardPrices) {
+            if (Math.abs(budget - keyboardPrice) < minDifference) {
+                minDifference = budget - keyboardPrice;
+                keyboardInBudget = keyboardPrice;
+            }
+        }
+        return keyboardInBudget;
+    }
+
+    public int cheapestUsb(int[] usbPrices) {
+        int minPrice = Integer.MAX_VALUE;
+
+        for (int usbPrice : usbPrices) {
+            if (usbPrice < minPrice) {
+                minPrice = usbPrice;
+            }
+        }
+        return minPrice;
+    }
+
+    public int bestCombinationInBudget(int budget, int[] keyboardPrices, int[] usbPrices) {
+        if (budget < this.cheapestUsb(usbPrices) || budget < this.cheapestKeyboard(keyboardPrices)) {
+            return -1;
+        }
+
+        if ((keyboardPrices.length == 1 && keyboardPrices[0] >= budget) || (usbPrices.length == 1 && usbPrices[0] >= budget)){
+            return -1;
+        }
+
+        int maxMoneySpent = 0;
+
+        for (int keyboardPrice : keyboardPrices) {
+            for (int usbPrice : usbPrices) {
+                int cost = keyboardPrice + usbPrice;
+
+                if (cost <= budget && cost > maxMoneySpent) {
+                    maxMoneySpent = cost;
+                }
+            }
+        }
+        return maxMoneySpent;
+    }
 }
